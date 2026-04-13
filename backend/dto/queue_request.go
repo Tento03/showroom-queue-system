241837f8cee2f64d4218caf51572eaefa4bd522a
Tento <regartento@gmail.com>
2026-04-13 14:08:11 +0700

    init project,add main,config db,add model and dto

---
+++ backend/dto/queue_request.go
@@ -0,0 +1,8 @@
+package dto
+
+type VCreateQueueRequest struct {
+	VehiclePlate    string `json:"vehicle_plate" binding:"required"`
+	VehicleImageURL string `json:"vehicle_image_url" binding:"required"`
+	OwnerName       string `json:"owner_name"`
+	OwnerPhone      string `json:"owner_phone"`
+}
---
+++ backend/models/queue.go
@@ -0,0 +1,13 @@
+package models
+
+import "time"
+
+type Queue struct {
+	ID              uint      `gorm:"primaryKey;autoIncrement" json:"id"`
+	QueueNumber     string    `gorm:"type:varchar(10);uniqueIndex" json:"queue_number"`
+	VehiclePlate    string    `gorm:"type:varchar(20);not null" json:"vehicle_plate"`
+	VehicleImageURL string    `gorm:"type:text;not null" json:"vehicle-image_url"`
+	OwnerName       string    `gorm:"type:varchar(100)" json:"owner_name"`
+	OwnerPhone      string    `gorm:"type:varchar(20)" json:"owner_phone"`
+	CreatedAt       time.Time `gorm:"autoCreateTime" json:"created_at"`
+}

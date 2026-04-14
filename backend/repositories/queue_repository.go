07447edba5ad4f40875f8330903bd2c08f877c73
Tento <regartento@gmail.com>
2026-04-14 08:34:30 +0700

    add queue repository and update queue model

---
+++ backend/repositories/queue_repository.go
@@ -0,0 +1,34 @@
+package repositories
+
+import (
+	"backend-queue/config"
+	"backend-queue/models"
+	"fmt"
+
+	"github.com/google/uuid"
+)
+
+type QueueRepository struct{}
+
+func NewQueueRepository() *QueueRepository {
+	return &QueueRepository{}
+}
+
+func (r *QueueRepository) CountTodayQueues() (int, error) {
+	var count int64
+	result := config.DB.Model(&models.Queue{}).Where("DATE(created_at)=CURDATE()").Count(&count)
+	if result.Error != nil {
+		return 0, fmt.Errorf("CountTodayQueues: %w", result.Error)
+	}
+	return int(count), nil
+}
+
+func (r *QueueRepository) CreateQueue(queue *models.Queue) error {
+	queue.ID = uuid.New().String()
+
+	result := config.DB.Create(queue)
+	if result.Error != nil {
+		return fmt.Errorf("CreateQueue: %w", result.Error)
+	}
+	return nil
+}
